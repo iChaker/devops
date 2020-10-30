08cd19f3845f12fa9b0be115d3a1b8d893e648b4
Ubuntu <ubuntu@ip-172-31-94-240.ec2.internal>
2020-10-30 14:00:48 +0000

    added factorial

--- ram.py
+++ ram.py
@@ -1,8 +1,17 @@
 import flask
 import psutil
 from hurry.filesize import size
+from math import factorial
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+
+@app.route('/fact<n>')
+def fact(n):
+	try:
+		return str(factorial(int(n)))
+	except:
+		return "error, please provide an int"
 
 
 @app.route('/ramusage', methods=['GET'])
